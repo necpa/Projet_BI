11ec79a1bef44f597e1db76ca48e85c9cb16002c
ELgagadhkt <61547651+ELgagadhkt@users.noreply.github.com>
2025-02-25 22:42:15 +0100

    Le formulaire marche et s'affiche correctement

--- webapp/web.py
+++ webapp/web.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello World"
+@app.route("/", methods=["GET", "POST"])
+def form():
+    if request.method == "POST":
+        data = request.form
+        return render_template("result.html", data=data)
+    return render_template("form.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
