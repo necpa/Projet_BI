64eedd05fdb2c59f3d97357e7c56893c3d02282a
ELgagadhkt <61547651+ELgagadhkt@users.noreply.github.com>
2025-02-28 23:52:35 +0100

    C'est bon, on peut importer un fichier csv et remplir un remplir un formulaire

--- webapp/web.py
+++ webapp/web.py
@@ -1,13 +1,38 @@
+import pandas as pd
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET", "POST"])
 def form():
+    data = []
     if request.method == "POST":
-        data = request.form
+        form_type = request.form.get("form_type")
+        # Vérifier si un fichier a été uploadé
+        if form_type == "file_upload" and "file" in request.files and request.files["file"].filename != "":
+            file = request.files["file"]
+
+            try:
+                # Lire le fichier CSV
+                df = pd.read_csv(file)  # On peut ajouter sep=";" si le fichier utilise des ";"
+                # Convertir en liste de dictionnaires pour affichage
+                data = df.to_dict(orient="records")
+            except Exception as e:
+                return f"Erreur lors de la lecture du fichier : {str(e)}"
+
+
+
+        # Vérifier si le formulaire manuel est soumis
+        elif form_type == "manual_entry":
+            form_data = request.form.to_dict()
+            data.append(form_data)
+
         return render_template("result.html", data=data)
+
     return render_template("form.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
